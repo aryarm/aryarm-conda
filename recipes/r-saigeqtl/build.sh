594a00f9cbfd19d789d957d7fddc3dff3a8c729a
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2025-09-16 08:12:47 -0700

    try copilot suggestion of dynamically detecting gcc and darwin version

--- recipes/r-saigeqtl/build.sh
+++ recipes/r-saigeqtl/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [[ "$(uname -m)" == "arm64" ]]; then
+    # let's dynamically detect the GCC and Darwin version in the gfortran library path
+    GCC_LIB_PATH=$(find "${PREFIX}/lib/gcc/" -type d -name "arm64-apple-darwin*" | head -n 1)
+    GCC_VERSION=$(basename "$(find "${GCC_LIB_PATH}" -mindepth 1 -maxdepth 1 -type d | sort | tail -n 1)")
+    # hard gfortran paths in R build(?) break the build on osx-arm64
+    sed -i.bak "s|\\\$(FLIBS)|-L${GCC_LIB_PATH}/${GCC_VERSION} -lgfortran -lquadmath -lm -L\${PREFIX}/lib/R/lib -lR -Wl,-framework -Wl,CoreFoundation|" src/Makevars
+fi
+
 $R CMD INSTALL --build .
 
 pushd extdata
